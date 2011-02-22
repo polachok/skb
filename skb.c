04559b558e06344758a77e77d1943a6b1503cc6f
Alexander Polakov <polachok@gmail.com>
2011-02-22 20:54:15 +0300

    indent

--- skb.c
+++ skb.c
@@ -17,12 +17,12 @@
 void
 eprint(const char *errstr, ...)
 {
-	va_list ap;
+    va_list ap;
 
-	va_start(ap, errstr);
-	vfprintf(stderr, errstr, ap);
-	va_end(ap);
-	exit(EXIT_FAILURE);
+    va_start(ap, errstr);
+    vfprintf(stderr, errstr, ap);
+    va_end(ap);
+    exit(EXIT_FAILURE);
 }
 
 int
